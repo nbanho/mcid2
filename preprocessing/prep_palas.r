bc78c11881e425871797e0ad119a3e3f80d0a4c6
nb22i345 <nb22i345@MAC-22-093-0.clients.ispm.unibe.ch>
2023-05-23 17:07:41 +0200

    manual fixes in epidemiological reporting

--- preprocessing/prep_palas.r
+++ preprocessing/prep_palas.r
@@ -7,8 +7,8 @@ library(lubridate)
 
 #### Data ####
 
-files_A <- list.files("data-raw/palas/Palas Wochen 1-3/E3a_3 Palas_AQGuard_SN15988/aqGuard_15988", full.names = T)
-files_B <- list.files("data-raw/palas/Palas Wochen 1-3/E3b_3 Palas_AQGuard_SN15973/aqGuard_15973", full.names = T)
+files_A <- list.files("data-raw/palas/E3a_komplett_aqGuard_15988", full.names = T)
+files_B <- list.files("data-raw/palas/E3b_komplett_aqGuard_15973", full.names = T)
 files_A <- files_A[grepl("txt", files_A)]
 files_B <- files_B[grepl("txt", files_B)]
 
@@ -37,6 +37,17 @@ df <- rbind(data.frame(class = "A", file = files_A),
 
 #### Preprocessing ####
 
+# remove weekends and vacation
+df <- df %>%
+  filter(!(weekdays(date) %in% c("Saturday", "Sunday")),
+         !(date %in% seq.Date(as.Date("2023-02-06"), as.Date("2023-02-11"), by = "1 day")),
+         date >= as.Date("2023-01-16"),
+         date <= as.Date("2023-03-11")) 
+
+# move time in class B (E3b) on 2023-01-16 (first weekend, Monday) by one hour backwards (see data-collection-issues.docx)
+df$datetime[df$class=="B"&df$date==as.Date("2023-01-16")] <- df$datetime[df$class=="B"&df$date==as.Date("2023-01-16")] %m-% hours(1)
+df %>% filter(date == as.Date("2023-01-16")) %>% ggplot(aes(x = datetime, y = CO2, color = class)) + geom_line()
+
 to_hhmm <- function(t) {
   h <- hour(t)
   if (nchar(h) == 1) {
@@ -50,40 +61,207 @@ to_hhmm <- function(t) {
 }
 
 # Filter time when people are in class
-
 rtime <- readRDS("data-clean/time-in-room.rds") 
 rtime$hhmm <- sapply(rtime$datetime, to_hhmm)
-
-df_filt <- df 
-df_filt$hhmm <- sapply(df_filt$datetime, to_hhmm) 
-df_filt <- df_filt %>%
+ 
+df$hhmm <- sapply(df$datetime, to_hhmm) 
+df <- df %>%
   left_join(rtime %>% select(class, date, hhmm, no_palas)) %>%
+  mutate(palas = ifelse(is.na(no_palas), T, F)) 
+df_filt <- df %>%
   filter(!no_palas)
 
-saveRDS(df, "data-clean/palas.rds")
+saveRDS(df, "data-clean/palas-all.rds")
 
 
-#### Quick look ####
+#### Mean particle concentration ####
 
-df_filt_2nd  <- df_filt %>%
-  select(PM1, PM25, PM4, PM10, CO2, Cn) %>%
-  set_names(c("pm1mugm", "pm25mugm", "pm4mugm", "pm10mugm", "co2ppm", "cn1cm")) %>%
-  mutate(study = "2nd")
+df_daily_av <- df_filt %>%
+  group_by(class, date) %>%
+  summarize(across(c(Cn, PMtot, PM1, PM25, PM4, PM10, Temp, rH, CO2), ~ mean(.x))) %>%
+  ungroup()
 
-df_filt_1st <- read_csv("../mcid/data-clean/palas.csv") %>%
-  filter(school == "School 2") %>%
-  select(c("pm1mugm", "pm25mugm", "pm4mugm", "pm10mugm", "co2ppm", "cn1cm")) %>%
-  mutate(study = "1st")
 
-df_comp_12 <- rbind(df_filt_1st, df_filt_2nd) %>%
-  melt(c("study"))
+#### Air change rate ####
 
-comp_12_pl <- df_comp_12 %>%
-  ggplot(aes(color = study, y = value, x = study)) +
-  geom_boxplot() +
-  facet_wrap(~ variable, scales = "free_y") +
-  scale_color_brewer(palette = "Set1") +
-  theme(legend.position = "none", axis.title.y = element_blank()) 
+# subset data between 7 to 17 h
+vent_dat <- df %>%
+  group_by(class, date) %>%
+  slice(1) %>%
+  dplyr::select(class, date) %>%
+  mutate(datetime = map(date, function(x) seq.POSIXt(as.POSIXct(paste(x, "06:50:00")), as.POSIXct(paste(x, "17:10:00")), by = "1 min"))) %>%
+  unnest(datetime) %>%
+  left_join(df %>% dplyr::select(class, datetime, CO2, no_palas)) %>%
+  mutate(no_palas = ifelse(is.na(no_palas), T, no_palas)) %>%
+  group_by(class, date) %>%
+  arrange(datetime) %>%
+  mutate(CO2 = zoo::na.approx(CO2, na.rm = F, maxgap = Inf)) %>%
+  tidyr::fill(CO2, .direction = "downup") %>%
+  ungroup() 
 
-comp_12_pl
-ggsave("results/comparison_studies_palas.pdf", width = 16 / cm(1), height = 12 / cm(1))
+
+# add number of absences
+epi_dat <- readRDS("data-clean/epidemiological.rds")
+vent_dat <- vent_dat %>%
+  left_join(epi_dat %>% dplyr::select(date, class, n_present)) %>%
+  mutate(n_present = ifelse(no_palas, 0, n_present))
+
+# linear increase/decrease 10min before/after room is supposed to be vacated
+linear_impute <- function(x, y) {
+  lag_y <- lag(y, 10)
+  lag_y <- ifelse(is.na(lag_y), T, lag_y)
+  lead_y <- lead(y, 10)
+  lead_y <- ifelse(is.na(lead_y), T, lead_y)
+  x_na <- ifelse(!y, x,
+              ifelse(!lag_y, NA,
+                     ifelse(!lead_y, NA, x)))
+  x_imp <- zoo::na.approx(x_na, na.rm = F, maxgap = Inf)
+  return(round(x_imp))
+}
+vent_dat <- vent_dat %>%
+  group_by(class, date) %>%
+  arrange(datetime) %>%
+  mutate(n_present_imp = linear_impute(n_present, no_palas)) %>%
+  ungroup() 
+
+# inspect
+# vent_dat %>%
+#   ggplot(aes(x = datetime, y = n_present_imp)) +
+#   geom_line() +
+#   facet_wrap(class ~ date, scales = "free_x")
+
+# add teacher during time in class (not outside)
+vent_dat <- vent_dat %>% 
+  mutate(n_present_imp = ifelse(!no_palas, n_present_imp + 1, n_present_imp))
+
+# average CO2 emission rate
+#' assume CO2 generation rate based on Persily et al. (2007): https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5666301/
+#' For students: assume 0.3 L/min (based on physical activity level met 1.6, age group 11 to 16, roughly the average of males and females)
+#' For teachers: assume 0.36 L/min (based on physical activity level met 1.6, roughly the average of age groups 30 to 60 of males and females)
+vent_dat <- vent_dat %>%
+  mutate(nG = ifelse(!no_palas, (n_present_imp-1) * 0.3 + n_present_imp * 0.36, n_present_imp * 0.3))
+
+# add volume 
+vent_dat$V <- 233
+
+# lead CO2
+vent_dat <- vent_dat %>%
+  rename(C = CO2) %>%
+  group_by(class, date) %>%
+  arrange(datetime) %>%
+  mutate(C1 = lead(C)) %>%
+  slice(-n()) %>%
+  ungroup() %>%
+  filter(hm(format(datetime, format = "%H:%M")) >= hm("07:00"),
+         hm(format(datetime, format = "%H:%M")) <= hm("17:00"))
+
+# add dt
+vent_dat$dt <- 1/60
+
+# estimate air exchange rate
+#' based on Equation 16, page 7, Batterman et al. (2017): https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5334699/
+transient_mass_balance <- function(Q, C, nG, V, dt, Cr) {
+  6 * 10^4 * nG / Q * (1 - exp(-Q / V * dt)) + (C - Cr) * exp(- Q / V * dt) + Cr
+}
+#' minimize residual sum of squares
+min_rss <- function(data, par) {
+  #' par[1] is the ventilation rate
+  #' par[2] is the outdoor CO2 level
+  with(data, sum( (C1 - transient_mass_balance(par[1], C, nG, V, dt, par[2])) ^ 2))
+}
+
+aer <- vent_dat %>%
+  group_by(class, date) %>%
+  nest() %>%
+  ungroup() %>%
+  #' assume ventilation rate to be between 0.01 (exact 0 not possible) and 400 and initialize with 1
+  #' assume Cr to be between 375 and 425 and estimate as well
+  mutate(results = lapply(data, function(X) optim(par = c(1, 400), fn = min_rss, data = X, lower = c(0.01, 375), upper = c(Inf, 425), method = "L-BFGS-B")),
+         Q = sapply(results, function(x) x$par[1]),
+         Cr = sapply(results, function(x) x$par[2]),
+         AER = Q / 233) 
+
+# inspect
+aer %>%
+  ggplot(aes(x = date, y = AER, color = class)) +
+  geom_point() +
+  geom_smooth()
+
+#### Combine data ####
+
+env_dat <- left_join(
+  aer %>% dplyr::select(class, date, AER),
+  df_daily_av %>% dplyr::select(class, date, Cn, PMtot, PM1, PM25, PM4, PM10, Temp, rH, CO2)
+) %>%
+  mutate(AER = ifelse(is.na(CO2), NA, AER),
+         aircleaner = ifelse(class == "A", 
+                             ifelse(date <= as.Date("2023-01-28"), "yes",
+                                    ifelse(date >= as.Date("2023-02-27"), "yes", "no")),
+                             ifelse(date <= as.Date("2023-01-28"), "no",
+                                    ifelse(date >= as.Date("2023-02-27"), "no", "yes"))),
+         weekday = weekdays(date)) %>%
+  dplyr::select(class, date, aircleaner, CO2, AER, Cn, PMtot, PM1, PM25, PM4, PM10, Temp, rH)
+  
+saveRDS(env_dat, "data-clean/environmental.rds")
+
+
+#### Check CO2 ####
+
+# co2_time_pl <- df %>%
+#   mutate(time = as.POSIXct(time, format = "%H:%M:%S")) %>%
+#   ggplot(aes(x = time, y = CO2, color = class)) + 
+#   geom_line() +
+#   geom_vline(aes(xintercept = as.POSIXct("2023-03-14 07:00:00")), linetype = "dashed") +
+#   geom_vline(aes(xintercept = as.POSIXct("2023-03-14 17:00:00")), linetype = "dashed") +
+#   geom_hline(aes(yintercept = 500), linetype = "dashed", color = "red") +
+#   scale_x_datetime(labels = scales::time_format(format = "%H:%M")) +
+#   facet_wrap(~ date, ncol = 5) +
+#   theme_bw() +
+#   theme(text = element_text(size = 8), axis.title = element_blank(),
+#         legend.position = "top", legend.direction = "horizontal")
+# co2_time_pl
+# ggsave(plot = co2_time_pl, filename = "results/checks/co2-timeseries.pdf", width = 21 / cm(1), height = 28 / cm(1))
+# 
+# co2_daily_pl <- df_full %>%
+#   mutate(weekday = weekdays(date),
+#          no_palas = ifelse(no_palas, "No School", "School")) %>%
+#   filter(!(weekday %in% c("Saturday", "Sunday")),
+#          hm(hhmm) >= hm("07:00"),
+#          hm(hhmm) <= hm("17:00")) %>%
+#   ggplot(aes(x = hhmm, y = CO2, color = no_palas)) +
+#   geom_point() +
+#   facet_wrap(class ~ date) 
+# co2_daily_pl
+# 
+# df %>%
+#   ggplot(aes(x = datetime, y = rH)) +
+#   geom_point(shape = 21, size = .5) +
+#   geom_line() +
+#   labs(y = "Relative humidity (%)") +
+#   theme_bw() +
+#   theme(axis.title.x = element_blank(), text = element_text(size = 8))
+# 
+# ggsave("results/humidity.png", width = 16 / cm(1), height = 10 / cm(1))
+# 
+# df_filt_2nd  <- df_filt %>%
+#   select(PM1, PM25, PM4, PM10, CO2, Cn) %>%
+#   set_names(c("pm1mugm", "pm25mugm", "pm4mugm", "pm10mugm", "co2ppm", "cn1cm")) %>%
+#   mutate(study = "2nd")
+# 
+# df_filt_1st <- read_csv("../mcid/data-clean/palas.csv") %>%
+#   filter(school == "School 2") %>%
+#   select(c("pm1mugm", "pm25mugm", "pm4mugm", "pm10mugm", "co2ppm", "cn1cm")) %>%
+#   mutate(study = "1st")
+# 
+# df_comp_12 <- rbind(df_filt_1st, df_filt_2nd) %>%
+#   melt(c("study"))
+# 
+# comp_12_pl <- df_comp_12 %>%
+#   ggplot(aes(color = study, y = value, x = study)) +
+#   geom_boxplot() +
+#   facet_wrap(~ variable, scales = "free_y") +
+#   scale_color_brewer(palette = "Set1") +
+#   theme(legend.position = "none", axis.title.y = element_blank()) 
+# 
+# comp_12_pl
+# ggsave("results/comparison_studies_palas.pdf", width = 16 / cm(1), height = 12 / cm(1))
--- preprocessing/prep_redcap.r
+++ preprocessing/prep_redcap.r
@@ -2,13 +2,14 @@
 
 library(tidyverse)
 library(reshape2)
+library(lubridate)
 
 
 #### Data ####
 
 df <- read.csv("data-raw/redcap/MCIDSchulenFollowup_DATA_2023-02-09_1356.csv") %>%
   mutate(class = ifelse(grepl("Kontroll", record_id), "A", "B"),  # A is Kontroll- and B is Studienklasse
-         date = as.Date(date, format = "%d.%m.%y")) 
+         date = as.Date(date, format = "%Y-%m-%d")) 
 
 
 #### In-Room time ####
@@ -36,3 +37,141 @@ rtime <- df %>%
 
 
 saveRDS(rtime, "data-clean/time-in-room.rds")
+write.csv(rtime, "data-clean/time-in-room.csv", row.names = F)
+
+
+#### Absences ####
+
+cases <- df %>%
+  dplyr::select(class, initialen_sus, matches("sus_")) %>%
+  filter(sus_date != "") %>%
+  rename(date_symptom = sus_symp_date,
+         date_absent = sus_date,
+         date_back = sus_date_back,
+         sex = sus_sex,
+         reason = sus_absenz,
+         student_id = initialen_sus) %>%
+  mutate(across(c(date_symptom, date_absent, date_back), as.Date),
+         sex = factor(ifelse(sex == 1, "male", "female"), levels = c("male", "female")),
+         reason = factor(ifelse(is.na(reason), "unknown", ifelse(reason == 1, "sickness", "other")), levels = c("sickness", "other", "unknown")),
+         respiratory_infection = `sus_symp___1` + `sus_symp___2` + `sus_symp___3` + `sus_symp___4` +
+                                       `sus_symp___5` + `sus_symp___6` + `sus_symp___7` + `sus_symp___8` +
+                                       `sus_symp___9` + `sus_symp___10` + `sus_symp___11` + `sus_symp___12` +
+                                       `sus_symp___13`,
+         respiratory_infection = ifelse(respiratory_infection >= 1, "yes", "no"),
+         respiratory_infection = ifelse(sus_testing == 1 & sus_tesing_res == 1, "no", respiratory_infection), # re-label one case that tested negative
+         respiratory_infection = factor(respiratory_infection, levels = c("no", "yes"))) %>% 
+  dplyr::select(class, student_id, date_symptom, date_absent, date_back, sex, reason, respiratory_infection) 
+
+# check delay between symptom onset and absence
+cases <- cases %>%
+  mutate(delay_absence = as.numeric(difftime(date_absent, date_symptom, units = "days"))) 
+View(cases)
+
+#' there was one case with 8 day delay between symptom onset and absence
+#' in the raw report it says "since Wednesday / 25.01.2023", 
+#' but the absence was on the 02.02.2023, so the last Wednesday would be 01.02.2023,
+#' which would only mean 1 day of delay. This seems more likely and we code it as such.
+
+cases$date_symptom[cases$delay_absence == 8] <- as.Date("2023-02-01")
+
+# check time absent
+cases <- cases %>%
+  mutate(time_absent = as.numeric(difftime(date_back, date_absent, units = "days"))) 
+View(cases)
+
+#' the following students were absent only in the morning (and return in the afternoon)
+#' nevertheless, we consider them as being absent for the whole day, 
+#' thus shifting their date back by one day
+cases$date_back[cases$class == "A" & cases$student_id == 7 & cases$date_back == as.Date("2023-01-19")] <- as.Date("2023-01-19") + days(1)
+cases$date_back[cases$class == "B" & cases$student_id == 6 & cases$date_back == as.Date("2023-01-26")] <- as.Date("2023-01-26") + days(1)
+cases$date_back[cases$class == "B" & cases$student_id == 15 & cases$date_back == as.Date("2023-02-15")] <- as.Date("2023-02-15") + days(1)
+cases$date_back[cases$class == "B" & cases$student_id == 19 & cases$date_back == as.Date("2023-02-23")] <- as.Date("2023-02-23") + days(1)
+cases$date_back[cases$class == "B" & cases$student_id == 12 & cases$date_back == as.Date("2023-03-02")] <- as.Date("2023-03-02") + days(1)
+
+#' the following students return on the same date of their absence
+#' but in the report the original date written with pencil,
+#' which was one day later, was overwritten with pen
+#' we assume that the more sensible pencil date is the correct one
+
+cases$date_back[cases$class == "A" & cases$student_id == 5 & cases$date_back == as.Date("2023-01-16")] <- as.Date("2023-01-16") + days(1)
+cases$date_back[cases$class == "A" & cases$student_id == 10 & cases$date_back == as.Date("2023-01-17")] <- as.Date("2023-01-17") + days(1)
+
+#' there is one missing case which will be set to no respiratory infection
+cases$respiratory_infection[cases$reason=="unknown"] <- "no"
+#' the date of absence will be imputed assuming the median duration of absence for all non respiratory infections
+med_dur_no <- median(difftime(cases$date_back[cases$respiratory_infection=="no"], 
+                              cases$date_absent[cases$respiratory_infection=="no"], 
+                              units = "days"), 
+                     na.rm = T)
+cases$date_back[cases$reason=="unknown"] <- cases$date_absent[cases$reason=="unknown"] + days(as.numeric(med_dur_no))
+
+# save long data
+cases <- cases %>% 
+  dplyr::select(-delay_absence, -time_absent)
+saveRDS(cases, "data-clean/redcap-long.rds")
+
+# compute absences 
+absences <- cases %>%
+  mutate(date = map2(date_absent, date_back, function(x, y) if (x == y) { NA } else { seq.Date(x, y - days(1), by = "1 day") })) %>% # the if else clause is not necessary anymore after the above preprocessing steps
+  unnest(date) %>%
+  filter(!is.na(date)) %>%
+  group_by(class, date) %>%
+  summarize(n_absent = n()) %>%
+  ungroup()
+
+# compute respiratory cases
+resp_cases <- cases %>%
+  filter(respiratory_infection == "yes") %>%
+  group_by(class, date_symptom) %>%
+  summarize(new_cases = n()) %>%
+  ungroup() %>%
+  rename(date = date_symptom)
+
+# full join
+epi_data <- data.frame(date = rep(seq.Date(as.Date("2023-01-16"), as.Date("2023-03-11"), by = "1 day"), 2)) %>%
+  mutate(class = rep(c("A", "B"), each = nrow(.) / 2),
+         n_class = rep(c(20, 17), each = nrow(.) / 2),
+         weekday = weekdays(date),
+         weekend = ifelse(weekday %in% c("Saturday", "Sunday"), 1, 0),
+         vacation = ifelse(date %in% seq.Date(as.Date("2023-02-06"), as.Date("2023-02-11"), by = "1 day"), 1, 0),
+         aircleaner = ifelse(class == "A", 
+                             ifelse(date <= as.Date("2023-01-28"), "yes",
+                                    ifelse(date >= as.Date("2023-02-27"), "yes", "no")),
+                             ifelse(date <= as.Date("2023-01-28"), "no",
+                                    ifelse(date >= as.Date("2023-02-27"), "no", "yes"))),
+         aircleaner = ifelse(weekend + vacation > 0, "no", aircleaner)) %>%
+  mutate(n_class = ifelse(class == "A" & date > as.Date("2023-02-11"), 18, n_class)) %>% # class A was 17 students before and 18 after the vacation
+  left_join(absences) %>%
+  left_join(resp_cases) %>%
+  mutate(new_cases = ifelse(is.na(new_cases), 0, new_cases),
+         n_absent = ifelse(is.na(n_absent), 0, n_absent),
+         n_present = n_class - n_absent) %>%
+  dplyr::select(class, n_class, date, weekday, weekend, vacation, aircleaner, n_absent, n_present, new_cases)
+
+saveRDS(epi_data, "data-clean/epidemiological.rds")
+
+
+#### Quick Look ####
+cases %>% 
+  group_by(class, reason, respiratory_infection) %>%
+  summarize(n = n())
+
+cases %>%
+  filter(respiratory_infection == "yes") %>%
+  mutate(weekday = weekdays(date_symptom)) %>%
+  group_by(weekday) %>%
+  summarize(n = n())
+
+cases %>%
+  filter(respiratory_infection == "yes") %>%
+  mutate(dd = date_absent - date_symptom) %>%
+  ggplot(aes(x = dd)) +
+  geom_histogram()
+
+cases %>%
+  filter(respiratory_infection == "yes") %>%
+  mutate(dd = date_back - date_absent) %>%
+  ggplot(aes(x = dd)) +
+  geom_histogram()
+  
